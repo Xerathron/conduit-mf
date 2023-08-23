68d3460b0f0c9450b7a8ff5323d1343975e286a6
Matthias Feyll <matthias.feyll@stud.h-da.de>
2023-08-23 10:47:16 +0200

    [Step1]: build scaffold

--- projects/shell/src/app/app.component.spec.ts
+++ projects/shell/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-name'`, () => {
+  it(`should have as title 'shell'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-name');
+    expect(app.title).toEqual('shell');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('project-name app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('shell app is running!');
   });
 });
